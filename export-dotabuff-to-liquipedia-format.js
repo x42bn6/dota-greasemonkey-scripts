cb46300426f68c0c38736059311d2d28a956f027
x42bn6 <x42bn6@gmail.com>
2024-11-14 20:57:14 +0000

    Add winner icon to make it easier if we know who the winner is but don't know which team is which (e.g. nobody picked the correct team in the lobby)

--- export-dotabuff-to-liquipedia-format.js
+++ export-dotabuff-to-liquipedia-format.js
@@ -6,11 +6,22 @@
 // @include     https://www.dotabuff.com/matches/*
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js
 // ==/UserScript==
+var getWinner = function() {
+  $("section").each(function() {
+    var clazz = $(this).attr("class")
+    if (clazz === "radiant" || clazz === "dire") {
+      if ($(this).find("header span.victory-icon")[0]) {
+        return $(this).attr("class");
+      }
+    }
+  });
+}
+
 var generateOutput = function(flip) {
   var i = 0;
   var sides = [];
   var mapNumber = 1;
-  var winner = 0;
+  var winner = getWinner();
   var teamIndex = 1;
   var pickIndex = 1;
   var banIndex = 1;
@@ -19,16 +30,6 @@ var generateOutput = function(flip) {
   if ($("div.game-link.active").length > 0) {
     mapNumber = $("div.game-link.active a").text().split(" ")[1]
   }
-  
-  $("section").each(function() {
-    var clazz = $(this).attr("class")
-    if (clazz === "radiant" || clazz === "dire") {
-      sides.push(clazz);
-      if ($(this).find("header span.victory-icon")[0]) {
-        winner = $(this).attr("class");
-      }
-    }
-  });
   
   var s = "|map" + mapNumber + "={{Map\n";
 
@@ -132,10 +133,19 @@ $(document).ready(function() {
       teamB = $(teamSelector("dire")).text();
     }
     
-    return [ teamA, teamB ];
+    var winner = getWinner();
+    
+    return [ teamA, teamB, winner ];
   }
   let teamA, teamB;
-  [teamA, teamB] = getTeams();
+  [teamA, teamB, winner] = getTeams();
+  
+  var winnerIcon = " <i class=\"fa fa-trophy\"></i>";
+  if (winner === teamA) {
+    teamA += winnerIcon;
+  } else {
+    teamB += winnerIcon;
+  }
   
   var normal = $("<a>", {
     class: "esports-team esports-link team-link",
