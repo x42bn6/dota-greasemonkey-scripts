09e748aa2cd7ff65a91e405606258c0d1ab68f40
x42bn6 <x42bn6@gmail.com>
2025-01-09 18:47:52 +0000

    Add click-to-copy functionality for FACEIT script

--- faceit-player-id-shower.js
+++ faceit-player-id-shower.js
@@ -30,13 +30,35 @@ $(document).ready(function() {
     var addIds = function(id64) {
       var id32 = Number(BigInt(id64) - BigInt("76561197960265728"));
       var secondComponent = BigInt(id64) % 2n;
-      return "<div style=\"font-size: 90%;\">" + id32 + "<br/>" + id64 + "<br/>STEAM_0:" + secondComponent + ":" + Math.floor(id32/2) + "</div>";
+      return "<div style=\"font-size: 90%;\"><span class=\"steam-id\">" + id32 + "</span><br/><span class=\"steam-id\">" + id64 + "</span><br/><span class=\"steam-id\">STEAM_0:" + secondComponent + ":" + Math.floor(id32/2) + "</span></div>";
     }
     $(selector).each(function() {
       var hrefSplit = $(this).attr("href").split("/");
       var id64 = hrefSplit[hrefSplit.length-1];
       var append = addIds(id64);
+      console.log(append);
       $(this).parent().append(append);
     });
+  });
+
+  var lastClickedDiv = null;
+  $(document).on('click', '.steam-id', function() {
+    var content = $(this).clone()
+    .find('.tick').remove()
+    .end()
+    .text();
+
+    navigator.clipboard.writeText(content).catch(function(err) {
+      console.error('Error copying to clipboard: ', err);
+    });
+
+    if (lastClickedDiv) {
+      lastClickedDiv.find('.tick').remove();
+    }
+
+    var tick = "<span class='tick' style='color: green; margin-left: 5px;'>✔</span>";
+    $(this).append(tick);
+
+    lastClickedDiv = $(this);
   });
 });
