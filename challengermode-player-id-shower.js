26ec9d7bf3bdddb3c0ce5bf13c8d6e304d638b2a
x42bn6 <x42bn6@gmail.com>
2025-12-28 20:58:11 +0000

    Update Challengermode ID shower

--- challengermode-player-id-shower.js
+++ challengermode-player-id-shower.js
@@ -10,7 +10,9 @@ $(document).ready(function() {
   var addIds = function(id64) {
     var id32 = Number(id64.substr(-16,16)) - 6561197960265728;
     var secondComponent = BigInt(id64) % 2n;
-    return "<div style=\"font-size: 75%;\">" + id32 + "<br/>" + id64 + "<br/>STEAM_0:" + secondComponent + ":" + Math.floor(id32/2) + "</div>";
+    return "<div><span class='steam-id' style='font-size: 90%; cursor: pointer;' title='Click to copy'>" + id32 + "</span><br/>" +
+           "<span class='steam-id' style='font-size: 90%; cursor: pointer;' title='Click to copy'>" + id64 + "</span><br/>" +
+           "<span class='steam-id' style='font-size: 90%; cursor: pointer;' title='Click to copy'>" + "\nSTEAM_0:" + secondComponent + ":" + Math.floor(id32/2) + "</span></div>";
   }
   
   var waitForEl = function(selector, callback) {
@@ -23,12 +25,34 @@ $(document).ready(function() {
     }
   };
 
-  var selector = "span.ellipsis.dis--blk span a";
+  var selector = "a[href^=\"https://www.dotabuff.com\"]";
   waitForEl(selector, function() {
     $(selector).each(function() {
       var hrefSplit = $(this).attr("href").split("/");
       var id64 = hrefSplit[hrefSplit.length-1].split("-")[0];
-      $(this).parent().parent().parent().append(addIds(id64));
+      $(this).parent().append(addIds(id64));
     });
+  });
+  
+  var lastClickedDiv = null;
+
+  $(document).on('click', '.steam-id', function() {
+    var content = $(this).clone()
+      .find('.tick').remove()
+      .end()
+      .text();
+
+    navigator.clipboard.writeText(content).catch(function(err) {
+      console.error('Error copying to clipboard: ', err);
+    });
+
+    if (lastClickedDiv) {
+      lastClickedDiv.find('.tick').remove();
+    }
+
+    var tick = "<span class='tick' style='color: green; margin-left: 5px;'>✔</span>";
+    $(this).append(tick);
+
+    lastClickedDiv = $(this);
   });
 });
